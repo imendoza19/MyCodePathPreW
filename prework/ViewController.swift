8e16abdcbf61c24c39d97d6ab1d1eac2814e13a8
imendoza19 <110577664+imendoza19@users.noreply.github.com>
2022-08-12 22:24:18 -0500

    Added Prework

---
+++ prework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prework
+//
+//  Created by Isaac Mendoza on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    @IBAction func ButtonClicked(_ sender: Any)     {
+        print("Hello")
+        TextLabel.textColor = UIColor.orange
+    }
+}
+
